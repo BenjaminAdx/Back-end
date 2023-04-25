9546d1d49f249871f48678d1b507f637a61f6093
benjaminadx <andrieux.benjamin1@gmail.com>
2023-04-25 13:45:12 +0200

    autocomplete terminé

--- autocompletephp/search_data2.php
+++ autocompletephp/search_data2.php
@@ -7,15 +7,15 @@ try {
 
     echo $e->getMessage();
 }
+if (!empty($_POST["search"])) {
+    $search = $_POST["search"];
+    $select = $pdo->prepare("SELECT country_name FROM apps_countries WHERE country_name LIKE ? LIMIT 10");
+    $select->execute(array("%$search%"));
+    $responses = $select->fetchAll();
+    foreach ($responses as $response) {
+        $data[] = $response["country_name"];
+    }
 
-$search = $_POST["search"];
-$select = $pdo->prepare("SELECT country_name FROM apps_countries WHERE country_name LIKE ? LIMIT 10");
-$select->execute(array("%$search%"));
-$responses = $select->fetchAll();
-$data = array();
-foreach ($responses as $response) {
-    $data[] = $response["country_name"];
+    $json = json_encode($data, JSON_PRETTY_PRINT);
+    echo $json;
 }
-
-$json = json_encode($data, JSON_PRETTY_PRINT);
-echo $json;
