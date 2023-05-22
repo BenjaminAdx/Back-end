e1c6de2bb0544f25315da8da7e7a29c88176595d
benjaminadx <andrieux.benjamin1@gmail.com>
2023-05-22 10:07:34 +0200

    javascript en oo par codeium (test)

--- ECF/public/javascript/Factures.js
+++ ECF/public/javascript/Factures.js
@@ -1,57 +1,63 @@
-let url = window.location.href;
-let urlParts = url.split("/");
-let lastValue = urlParts[urlParts.length - 1];
 
-let urlFetch = "/Back-end/ECF/Factures/facture/" + lastValue;
+class Facture {
+    constructor() {
+        this.url = window.location.href;
+        this.urlParts = this.url.split("/");
+        this.lastValue = this.urlParts[this.urlParts.length - 1];
+        this.urlFetch = "/Back-end/ECF/Factures/facture/" + this.lastValue;
+        this.produits = [];
+        this.indexligne = 2;
+    }
 
-console.log(urlFetch);
+    fetchProduits() {
+        fetch(this.urlFetch, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            credentials: 'same-origin'
+        })
+            .then(response => response.json())
+            .then(produits => {
+                this.produits = produits;
+                this.addFirstLigne();
+                this.addLigneOnClick();
+            })
+            .catch(error => console.error(error));
+    }
 
-
-fetch(urlFetch, {
-    method: 'GET',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    credentials: 'same-origin'
-})
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (produits) {
+    addFirstLigne() {
         const ligneProduitEl = document.querySelector('.ligne_produit_1');
         const selectProduitEl = document.createElement('select');
         selectProduitEl.name = 'produit_1';
 
-        produits.forEach((produit) => {
+        this.produits.forEach((produit) => {
             const optionProduit = document.createElement('option');
             optionProduit.value = produit.id;
             optionProduit.innerText = produit.name;
             selectProduitEl.appendChild(optionProduit);
         });
+
         const quantiteInput = document.createElement('input');
         quantiteInput.type = 'number';
         quantiteInput.name = 'quantite_1';
         quantiteInput.required = true;
 
-
         ligneProduitEl.appendChild(selectProduitEl);
         ligneProduitEl.appendChild(quantiteInput);
+    }
 
-
-
-        let indexligne = 2;
-
+    addLigneOnClick() {
         const ajoutBtn = document.querySelector('.ajout_ligne');
 
         ajoutBtn.addEventListener('click', () => {
-
             const ligneProduit = document.createElement('div');
-            ligneProduit.classList.add('ligne_produit_' + indexligne);
+            ligneProduit.classList.add('ligne_produit_' + this.indexligne);
 
             const selectProduit = document.createElement('select');
-            selectProduit.name = 'produit_' + indexligne;
+            selectProduit.name = 'produit_' + this.indexligne;
 
-            produits.forEach((produit) => {
+            this.produits.forEach((produit) => {
                 const optionProduit = document.createElement('option');
                 optionProduit.value = produit.id;
                 optionProduit.innerText = produit.name;
@@ -60,7 +66,7 @@ fetch(urlFetch, {
 
             const quantiteInput = document.createElement('input');
             quantiteInput.type = 'number';
-            quantiteInput.name = 'quantite_' + indexligne;
+            quantiteInput.name = 'quantite_' + this.indexligne;
             quantiteInput.required = true;
 
             ligneProduit.appendChild(selectProduit);
@@ -69,6 +75,10 @@ fetch(urlFetch, {
             const formulaire = document.querySelector('form');
             formulaire.insertBefore(ligneProduit, ajoutBtn);
 
-            indexligne++;
+            this.indexligne++;
         })
-    });
+    }
+}
+
+const facture = new Facture();
+facture.fetchProduits();
